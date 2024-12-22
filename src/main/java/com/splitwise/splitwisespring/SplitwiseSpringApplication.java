ff73e33712af631349cb3e132264dde257c6c12e
Anushka <anushkadoshi21@gmail.com>
2024-12-22 16:48:17 -0500

    Splitwise App

---
+++ src/main/java/com/splitwise/splitwisespring/SplitwiseSpringApplication.java
@@ -0,0 +1,14 @@
+package com.splitwise.splitwisespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SplitwiseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseSpringApplication.class, args);
+    }
+
+}
